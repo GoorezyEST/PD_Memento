9603f9a4ec9857d6fdf772530f7348023d00edc9
GoorezyEST <goorezyestudios@gmail.com>
2023-02-13 17:59:24 -0300

    Agregar archivos de proyecto.

---
+++ Caretaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento_PD
+{
+    // The 'Caretaker' class
+    internal class Caretaker
+    {
+        private List<Memento> _mementoList = new List<Memento>();
+
+        public void Add(Memento state)
+        {
+            _mementoList.Add(state);
+        }
+
+        public Memento Get(int index)
+        {
+            return _mementoList[index];
+        }
+    }
+}
---
+++ Memento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento_PD
+{
+    // The 'Memento' class
+    internal class Memento
+    {
+        private string _state;
+
+        // Constructor
+        public Memento(string state)
+        {
+            this._state = state;
+        }
+
+        // Property
+        public string State
+        {
+            get { return _state; }
+        }
+    }
+}
